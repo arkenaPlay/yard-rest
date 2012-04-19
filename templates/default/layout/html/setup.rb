3b61d7d3f8212532dce88c86eef43e082e9ebeb4
Chris Trinh <chris@synctv.com>
2012-04-19 12:38:36 -0700

    Add README and listing of resources to index.html

--- templates/default/layout/html/setup.rb
+++ templates/default/layout/html/setup.rb
@@ -1,8 +1,44 @@
 include Helpers::FilterHelper
 
+# Implementation from yard gem; couldn't use Object#super since we need to concat :diskfile and :index
 def init
-  @page_title = options[:title]
-  super
+  @breadcrumb = []  
+  if @onefile
+    sections :layout
+  elsif @file
+    if @file.attributes[:namespace]
+      @object = options.object = Registry.at(@file.attributes[:namespace]) || Registry.root 
+    end
+    @breadcrumb_title = "File: " + @file.title
+    @page_title = options[:title]
+    sections :layout, [:diskfile, :index]
+  elsif @contents
+    sections :layout, [:contents]
+  else
+    case object
+    when '_index.html'
+      @page_title = options[:title]
+      sections :layout, [:index, [:listing, [:files, :objects]]]
+    when CodeObjects::Base
+      unless object.root?
+        cur = object.namespace
+        while !cur.root?
+          @breadcrumb.unshift(cur)
+          cur = cur.namespace
+        end
+      end
+    
+      @page_title = format_object_title(object)
+      type = object.root? ? :module : object.type
+      sections :layout, [T(type)]
+    end
+  end
+end
+
+def diskfile
+  @file.attributes[:markup] ||= markup_for_file('', @file.filename)
+  data = htmlify(@file.contents, @file.attributes[:markup])
+  "<div id='filecontents' class='service'><h1>#{@file.title}</h1>" + data + "</div>"
 end
 
 def javascripts
@@ -15,9 +51,7 @@ def menu_lists
     { :type => 'file', :title => "Files", :search_title => "File List" } ]
 end
 
-
 def index
-
   legitimate_objects = @objects.select { |o| o.has_tag?('url') }
   @topics = {}
 
@@ -26,10 +60,6 @@ def index
   end
 
   @resources = legitimate_objects.sort_by {|o| o.tags('url').first.text }
-
-  @overall_objects = @objects.find_all {|o| o.has_tag?('overall')}.sort_by {|o| o.tag('overall').text}
-
+  @overall_objects = @objects.find_all { |o| o.has_tag?('overall') }.sort_by { |o| o.tag('overall').text }
   erb(:index)
 end
-
-
