3e509798ff6a858b44da70edb5b20eb7b6daf368
Chris Trinh <chris.chtrinh@gmail.com>
2012-05-21 19:17:48 -0700

    Add anchor tags to resource route links

--- lib/yard-rest/routes.rb
+++ lib/yard-rest/routes.rb
@@ -2,9 +2,20 @@ module YARD::Rest
   class Routes 
     class << self
       def with_resource_links(resources)
+        resource_path = ""
+        resource_index = 0
+
         all_routes = Rails.application.routes.routes # Journey::Routes object
         all_routes.to_a.map do |route|
-          {:verb => verb(route), :path => path(route), :link_object => resource(route, resources)}
+          link_object = resource(route, resources)
+          resource_index = if(!link_object.nil? && link_object.path == resource_path)
+            resource_index + 1
+          elsif !link_object.nil? && !link_object.path.nil?
+            resource_path = link_object.path
+            0
+          end
+
+          {:verb => verb(route), :path => path(route), :link_object => link_object, :resource_index => resource_index}
         end.reject do |route_hash| 
           route_hash[:link_object].nil? || route_hash[:verb].blank?
         end
